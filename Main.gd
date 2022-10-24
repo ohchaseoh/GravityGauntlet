737a530f848883eb204e38bf9da053a8bd1f60c0
sakata24 <97773052+sakata24@users.noreply.github.com>
2022-10-24 03:03:32 -0700

    added camera
    added dynamic camera, may need to fix angle

--- Main.gd
+++ Main.gd
@@ -1,7 +1,11 @@
 extends Spatial
 var ROT_SPEED = 0.0001
 var direction = Vector3.FORWARD
-func _process(delta):
 
+signal player_pos(p1_pos, p2_pos)
+
+func _process(delta):
+	
+	emit_signal("player_pos", $Player.translation, $Player2.translation)
 	$StaticBody.rotate(Vector3(1, 0, 0), ROT_SPEED)
 	$StaticBody.rotate(Vector3(0, 0, 1), ROT_SPEED)
